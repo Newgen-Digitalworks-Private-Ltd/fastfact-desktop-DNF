afa65928b56f63b1a387c540fec3af8e1eafc913
anuradhasulakhe <anuradha@fastfacts.co>
2023-12-07 16:15:03 +0530

    Jenkins_07Dec23

---
+++ run.bat
@@ -0,0 +1,8 @@
+@echo OFF 
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" x86
+echo "Starting Build for all Projects with proposed changes"
+echo .  
+devenv "D:\Jenkins\.jenkins\workspace\DNF\DNF.sln" /Rebuild Debug
+echo . 
+echo "All builds completed." 
+pause
