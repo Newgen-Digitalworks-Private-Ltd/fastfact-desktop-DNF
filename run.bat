5f7ac04e44917dd9c39790c22bf01627410d44dd
anuradhasulakhe <anuradha@fastfacts.co>
2023-12-08 12:41:54 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" x86
 echo "Starting Build for all Projects with proposed changes"
 echo .  
-devenv "D:\Jenkins\.jenkins\workspace\DNF\DNF.sln" /Rebuild Debug
+devenv "D:\Jenkins\.jenkins\workspace\DNF\eTdsDNF2223\DNF.sln" /Rebuild Debug
 echo . 
 echo "All builds completed." 
 pause
